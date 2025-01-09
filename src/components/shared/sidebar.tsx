1c3df9f80ea969a3d00db017de97fc756c068be9
Munim-Ali <munimali52@gmail.com>
2025-01-09 19:28:39 +0500

    Layout setup

---
+++ src/components/shared/sidebar.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="w-[300px] h-screen min-h-screen bg-white p-6">
+        Siderbar
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
