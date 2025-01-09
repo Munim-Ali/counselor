d5c8ec78c896ad4dcba870f698a3c411e224c678
Munim-Ali <munimali52@gmail.com>
2025-01-09 19:32:57 +0500

    Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Sidebar from "@/components/shared/sidebar";
+import Navbar from "@/components/shared/navbar";
 
 const clashDisplay = localFont({
   src: [
@@ -46,10 +47,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${clashDisplay.className} bg-[#F8F8F8] text-black flex items-center justify-between antialiased`}
+        className={`${clashDisplay.className} bg-[#F8F8F8] text-black flex items-start justify-between antialiased`}
       >
         <Sidebar />
-        <main className="w-full p-6">{children}</main>
+        <main className="w-full p-6">
+          <Navbar />
+          {children}
+        </main>
       </body>
     </html>
   );
---
+++ src/components/shared/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full flex items-center justify-end py-5 mb-4">
+        Alis Elsvin
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
